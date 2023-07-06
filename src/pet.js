ecc16ee04b75cdaa9bcf11234384fd92c229ec4e
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-07-06 20:44:40 +0100

    Added the Grow-Up method to my virtual pet.

--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,11 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
+    this.breed = 'Border Collie';
 }
+
+Pet.prototype.growUp = function() {
+  this.age += 6;
+};
 
 module.exports = Pet;
